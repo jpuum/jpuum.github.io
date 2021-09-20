24fdeb449b76e0bcb31478a164c170f1d6366592
jpuum <puumalajoram@gmail.com>
2021-09-20 17:17:56 +0300

    autogen: update site

--- update_site.sh
+++ update_site.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-jekyll build #--incremental
+jekyll build --incremental
 git --git-dir=.git --work-tree=_site add --all
 git --git-dir=.git --work-tree=_site commit -m "autogen: update site"
 git --git-dir=.git --work-tree=_site push
